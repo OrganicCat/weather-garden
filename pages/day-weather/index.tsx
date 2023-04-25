5aa490e686eb2472c8608e90736b966e18c17da6
Lee Delarm <lee.delarm@gmail.com>
2023-04-25 10:02:40 -0400

    Gardening aspects added, border colors

--- pages/day-weather/index.tsx
+++ pages/day-weather/index.tsx
@@ -12,6 +12,7 @@ export function DayWeather({ weatherData }: { weatherData: WeatherDay }) {
     const backgroundColor = themeContext.theme === "dark" ? "bg-gray-700" : "bg-gray-200";
     const textColor = themeContext.theme === "dark" ? "text-white" : "text-black";
     const borderColor = themeContext.theme === "dark" ? "border-white" : "border-black";
+    const flippedBorderColor = themeContext.theme === "dark" ? "border-gray-200" : "border-gray-700";
 
     const getWeatherImage = (weatherDayType: String) => {
         switch (weatherDayType) {
@@ -29,16 +30,24 @@ export function DayWeather({ weatherData }: { weatherData: WeatherDay }) {
         }
     }
 
+    const gardeningRating = (weatherData: WeatherDay) => {
+        const temp = weatherData.temp.day;
+        const rain = weatherData.weather === "Rain" ? 1 : 0;
+        return temp > 50 && temp < 90 && rain === 0 ? <>Good &#128519;</> : <>Bad &#128557;</>;
+    }
+
+
     const weatherImage = getWeatherImage(weatherData.weather);
 
     return (
-        <div className={`border-2 p-4 w-40 ${backgroundColor} ${textColor}`}>
+        <div className={`border-2 p-4 w-40 ${backgroundColor} ${textColor} ${flippedBorderColor}`}>
             <div className="text-center" data-id="monthday">{monthDay}</div>
             <div className={`text-center border-b-2 ${borderColor}`}>{weekDay}</div>
             <div className="text-center text-2xl pt-4">{weatherData.temp.day} &deg;F</div>
             <div className="text-center py-4" data-id="weatherImage">{weatherImage}</div>
             <div className="text-xs">Low: {weatherData.temp.min} &deg;F</div>
             <div className="text-xs">High: {weatherData.temp.max} &deg;F</div>
+            <div className={`border-t-2 pt-2 text-center text-sm ${borderColor}`}>It's a {gardeningRating(weatherData)} day for gardening!</div>
         </div>
     )
 }
